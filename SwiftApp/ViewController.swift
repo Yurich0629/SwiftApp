ef51c1b40f36a76f3ee332dfb65f9244222462dd
Ilya Desiatow <140006151+Yurich0629@users.noreply.github.com>
2023-08-30 23:34:34 +0300

    Initial Commit

---
+++ SwiftApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Илья Десятов on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
