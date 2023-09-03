3e212988feb25f866a490c90a0df43f50316bdbd
CemenVy <semen.vydrin0108@gmail.com>
2023-09-03 14:25:01 +0400

    Added new struct AppStylesManager

---
+++ SwiftApp/AppStylesManagers/AppStylesManager.swift
@@ -0,0 +1,17 @@
+//
+//  AppStylesManager.swift
+//  SwiftApp
+//
+//  Created by Семен Выдрин on 03.09.2023.
+//
+
+
+import UIKit
+
+
+   
+struct AppTheme {
+    static var backgroundColor: UIColor = .white
+    static var textColor: UIColor = .black
+}
+
